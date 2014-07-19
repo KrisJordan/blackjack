1301466859b5803af0cc312649c288c4b23bbb07
Kris Jordan <krisjordan@gmail.com>
2014-07-19 18:15:10 -0400

    More cleanup

--- lib/kris_jordan/blackjack.rb
+++ lib/kris_jordan/blackjack.rb
@@ -7,6 +7,7 @@ require 'kris_jordan/blackjack/dealer'
 require 'kris_jordan/blackjack/player'
 require 'kris_jordan/blackjack/state'
 require 'kris_jordan/blackjack/round'
+require 'kris_jordan/blackjack/game_journal'
 require 'kris_jordan/blackjack/game'
 
 module KrisJordan
--- lib/kris_jordan/blackjack/game.rb
+++ lib/kris_jordan/blackjack/game.rb
@@ -2,96 +2,17 @@ require 'json'
 
 module KrisJordan::Blackjack
 
-  class GameJournal
-    JOURNAL_FILE = '.blackjack.aof'
-
-    def self.in_progress?
-      File.exist? JOURNAL_FILE
-    end
-
-    def self.resumed_game
-      game = nil
-      File.open(JOURNAL_FILE, "r") do |file|
-        index = 0
-        file.each_line do |line|
-          object = unmarshall(JSON[line])
-          if index == 0
-            game = object
-          else
-            game.take object
-          end
-          index += 1
-        end
-      end
-      game
-    end
-
-    def self.unmarshall(obj)
-      classname = obj["classname"]
-      args      = obj["args"].map do |arg| 
-        if arg.is_a? Hash
-          self.unmarshall(arg)
-        else
-          arg
-        end
-      end
-      Object::const_get(classname).send(:new,*args)
-    end
-
-    def self.begin game
-      clear_journal
-      write_journal game
-    end
-    
-    def self.clear
-      file = '.blackjack.aof'
-      if File.exist? file
-        File.delete('.blackjack.aof')
-      end
-    end
-
-    def self.write action
-      open('.blackjack.aof','a') do |f| 
-        json = action.to_json
-        json[:timestamp] = Time.now
-        f.puts JSON.generate json
-      end
-    end
-
-  end
-
   class Game
 
     attr_accessor :players, :deck, :round
 
     def initialize starting_players, starting_chips, decks_per_round
-
-      if starting_chips < 1
-        @starting_chips = 1
-      else
-        @starting_chips   = starting_chips
-      end
-
-      if starting_players < 1
-        @starting_players = 1
-      else
-        @starting_players = starting_players
-      end
-      @players = starting_players
-                  .times
-                  .map {|n| Player.new("Player #{n+1}", @starting_chips)} +
-                  [Dealer.new]
-
-      if decks_per_round <= 1
-        @decks_per_round  = 2
-      else
-        @decks_per_round  = decks_per_round
-      end 
-      @deck             = Deck.new * @decks_per_round
-
-      @round            = Round.new @deck, @players
-
+      initialize_chips starting_chips
+      initialize_players starting_players
+      initialize_deck decks_per_round
+      @round = Round.new @deck, @players
     end
+
 
     def play(resume=false)
       GameJournal.begin(self) unless resume
@@ -104,6 +25,10 @@ module KrisJordan::Blackjack
       end
       GameJournal.clear
       puts "\nGame over, thanks for playing!\n\n"
+    end
+
+    def narrate description
+      puts "\n#{description}" if description
     end
 
     def take action
@@ -124,8 +49,36 @@ module KrisJordan::Blackjack
       ] }
     end
 
-    def narrate description
-      puts "\n#{description}" if description
+    private
+
+    def initialize_chips starting_chips
+      if starting_chips < 1
+        @starting_chips = 1
+      else
+        @starting_chips   = starting_chips
+      end
+    end
+
+    def initialize_players starting_players
+      if starting_players < 1
+        @starting_players = 1
+      else
+        @starting_players = starting_players
+      end
+      @players = starting_players
+                  .times
+                  .map {|n| Player.new("Player #{n+1}", @starting_chips)} +
+                  [Dealer.new]
+    end
+
+    def initialize_deck decks_per_round
+      if decks_per_round <= 1
+        @decks_per_round  = 2
+      else
+        @decks_per_round  = decks_per_round
+      end 
+      @deck             = Deck.new * @decks_per_round
+
     end
 
   end
